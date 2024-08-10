462d12c781494ba43efc1baadc720cf8faa3a2b8
신희성 <vvwv3092@gmail.com>
2024-08-10 10:23:17 +0900

    자바에서 제공하는 FunctionalInterface

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@ S
 #src/main/java/org/example/Main.java,0/b/0ba1b9c7a4878802f938e331cc2d8a042087ec6b
 [
 +src/main/java/org/example/RunSomething.java,7/2/7287e119e1c52e8b5891cef29557ca6ec9fbbcc8
+S
+#src/main/java/org/example/Plus.java,c/b/cbbf99935b80ee03651aa67c66b97121145e6017
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,45 @@
 package org.example;
 
+import java.util.Objects;
+import java.util.function.*;
+
 public class Main {
     public static void main(String[] args) {
-        // 익명 내부 클래스 anonymous inner class
-        RunSomething runSomething = new RunSomething() {
-            @Override
-            public int doIt(int number) {
-                return 10 + number;
-            }
-        };
+        Function<Integer, Integer> plusTen = (i) -> i + 10;
+        Function<Integer, Integer> multiplyTwo = (i) -> i * 2;
 
-        // 자바8 -> 람다 표현식
-        RunSomething runSomething2 = (number) -> 10 + number;
+        // function1.compose(function2) -> function2 and function1
+
+        Function<Integer, Integer> multiplyTwoAndPlusTen = plusTen.compose(multiplyTwo);
+        System.out.println(multiplyTwoAndPlusTen.apply(10));
+
+        // function1.andThen(function2) -> function1 and function2
+
+        Function<Integer, Integer> plusTenAndMultiplyTwo = plusTen.andThen(multiplyTwo);
+        System.out.println(plusTenAndMultiplyTwo.apply(10));
+
+        BiFunction<Integer, Integer, Integer> sumsum = (a, b) -> a + b;
+
+        // Consumer 리턴값이 void이다.
+        Consumer<String> logging = (i) -> System.out.println("log : " + i);
+        logging.accept("hello!");
+
+        // Supplier 파라미터를 받지 않고 반환값의 타입만 받는다. .get()
+        Supplier<Integer> getting = () -> 10;
+        System.out.println(getting.get());
+
+        Predicate<String> isAdmin = (name) -> Objects.equals(name, "admin");
+        System.out.println(isAdmin.test("user"));
+        System.out.println(isAdmin.test("admin"));
+
+
+//        Function<Integer, Integer> samesame = (i) -> i + 10;
+        // Function의 입력 값의 타입과 반환 값의 타입이 같을 때 -> UnaryOperator
+        UnaryOperator<Integer> samesame = (i) -> i + 10;
+
+
+//        BiFunction<Integer, Integer, Integer> sumsumsame = (a, b) -> a + b;
+        // BiFunction의 입력 값들의 타입과 반환 값의 타입이 같을 때 -> BinaryOperator
+        BinaryOperator<Integer> sumsumsame = (a, b) -> a + b;
     }
 }
